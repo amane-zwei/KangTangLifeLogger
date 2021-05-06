0269c7f0ab753df2d35ef92cba3ec9a2d5aef850
root-ame <roo10ame@gmail.com>
2021-05-07 02:45:31 +0900

    不要処理を削除

--- app/src/main/java/com/hoge/amazarashi/kangtanglifelogger/MainActivity.java
+++ app/src/main/java/com/hoge/amazarashi/kangtanglifelogger/MainActivity.java
@@ -1,24 +1,17 @@
 package com.hoge.amazarashi.kangtanglifelogger;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
-import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.hoge.amazarashi.kangtanglifelogger.fragments.InputFragment;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
-
-    List<OnPermissionResponse> onPermissionResponses = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,23 +31,5 @@ public class MainActivity extends AppCompatActivity {
 
             transaction.commit();
         }
-    }
-
-    public void addPermissionRequest(OnPermissionResponse onPermissionResponse) {
-        onPermissionResponses.add(onPermissionResponse);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        for (int index = onPermissionResponses.size() - 1; index >= 0; index--) {
-            OnPermissionResponse onPermissionResponse = onPermissionResponses.get(index);
-            if (onPermissionResponse.apply(requestCode, permissions, grantResults)) {
-                onPermissionResponses.remove(index);
-            }
-        }
-    }
-
-    public interface OnPermissionResponse {
-        boolean apply(int requestCode, String[] permissions, int[] grantResults);
     }
 }
